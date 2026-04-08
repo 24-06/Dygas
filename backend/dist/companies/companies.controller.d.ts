be98111fdcc044349ed86c328dcb39bf362cde8a
24-06 <mjcausil06@gmail.com>
2026-04-08 11:56:34 -0500

    cambios corregidos

--- backend/dist/companies/companies.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { CompaniesService } from './companies.service';
-export declare class CompaniesController {
-    private readonly companiesService;
-    constructor(companiesService: CompaniesService);
-    findAll(): Promise<import("./company.entity").Company[]>;
-    create(body: {
-        name: string;
-    }): Promise<import("./company.entity").Company>;
-}
--- backend/dist/users/users.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { UsersService } from './users.service';
-export declare class UsersController {
-    private readonly usersService;
-    constructor(usersService: UsersService);
-    findAll(): Promise<import("./user.entity").User[]>;
-    create(body: any, req: any): Promise<import("./user.entity").User>;
-}
