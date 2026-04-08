6059a862eae5b4fa22008f706b2f453979b37848
24-06 <mjcausil06@gmail.com>
2026-04-08 00:55:11 -0500

    cambios

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,18 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// En backend/src/main.ts
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // ESTA CONFIGURACIÓN ELIMINA CUALQUIER BLOQUEO DE COMUNICACIÓN
   app.enableCors({
-    origin: true, // Permite cualquier origen (Vercel, Localhost, Celular)
+    origin: true, // Esto permite que Vercel conecte sin problemas
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization, X-Requested-With',
   });
 
   const port = process.env.PORT || 3000;
-  // Escuchamos en 0.0.0.0 para que Railway sea accesible externamente
   await app.listen(port, '0.0.0.0');
 }
+
 bootstrap();
